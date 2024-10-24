dcc4054b16159b738d564b95fe8fac9095c71525
Matin Abaszada <81882048+MetinAbaszade@users.noreply.github.com>
2024-10-24 16:07:45 +0200

    Token problem solved in server

--- server.py
+++ server.py
@@ -51,24 +51,24 @@ def generate_jwt_token(user_id):
     token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
     return token
 
+
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         token = request.headers.get('Authorization')
-
         if not token:
             return jsonify({'message': 'Token is missing!'}), 401
 
         try:
-            token = token.split(" ")[1]  # Bearer token
-            data = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
-            current_user_id = data['userId']
+            # Extract token from 'Bearer token'
+            token = token.split(" ")[1] 
+            jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
         except jwt.ExpiredSignatureError:
             return jsonify({'message': 'Token has expired!'}), 401
         except jwt.InvalidTokenError:
             return jsonify({'message': 'Invalid token!'}), 401
 
-        return f(current_user_id, *args, **kwargs)
+        return f(*args, **kwargs)
 
     return decorated
 
